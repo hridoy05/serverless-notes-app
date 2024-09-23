d0ac74043523c94fb139dc7fefa47e7cca760ecb
hridoy05 <hridoy.banik71@gmail.com>
2024-09-23 22:04:31 +0600

    separte resource file

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ## install serverless
  - npm install --save-dev serverless@3.38
-
+## resource issue
+-         Resource:
+          - Fn::GetAtt: [notesTable, Arn]
 ## serverless project create
  - npm run sls -- create -t aws-nodejs
 
@@ -10,6 +12,13 @@
 ## configure aws cli
  - npm run sls -- cofig credentials --provider aws --key [key] --secret [key]
 
-# dynamodb steps
+## dynamodb steps
  - use clooudformation in serverless.yml file
  - use resources key word
+ - install @aws-sdk/lib-dynamodb for simplify.
+ - install yarn add @aws-sdk/client-dynamodb
+  
+## add plugin for fine grained permission on lembda
+ - install serverless-iam-roles-per-function
+  
+## deployment : setting up multiple environment
--- handler.js
+++ handler.js
@@ -1,32 +1,91 @@
-'use strict';
+"use strict";
+const DynamoDB = require("@aws-sdk/client-dynamodb");
+const {
+  DynamoDBDocumentClient,
+  PutCommand,
+  UpdateCommand,
+  DeleteCommand,
+  ScanCommand,
+} = require("@aws-sdk/lib-dynamodb");
 
-module.exports.createNote = async (event) => {
+const client = new DynamoDB({ region: process.env.AWS_REGION });
+const ddbDocClient = DynamoDBDocumentClient.from(client);
+const NOTES_TABLE_NAME = process.env.NOTES_TABLE_NAME;
+
+const send = (statusCode, data) => {
   return {
-    statusCode: 201,
-    body: JSON.stringify("create new notes"),
+    statusCode,
+    body: JSON.stringify(data),
   };
 };
-
-module.exports.updateNote = async (event) => {
-  const notesId = event.pathParameters.id
-  return {
-    statusCode: 200,
-    body: JSON.stringify(`update new notes${notesId} successfully`),
-  };
+module.exports.createNote = async (event, context, cb) => {
+  let data = JSON.parse(event.body);
+  try {
+    const params = {
+      TableName: NOTES_TABLE_NAME,
+      Item: {
+        notesId: data.id,
+        title: data.title,
+        body: data.body,
+      },
+      ConditionExpression: "attribute_not_exists(notesId)",
+    };
+    await ddbDocClient.send(new PutCommand(params));
+    return send(201, data);
+  } catch (err) {
+    return send(500, data);
+  }
 };
 
+module.exports.updateNote = async (event, context, cb) => {
+  let notesId = event.pathParameters.id;
+  let data = JSON.parse(event.body);
+  try {
+    const params = {
+      TableName: NOTES_TABLE_NAME,
+      Key: { notesId },
+      UpdateExpression: "set #title = :title, #body = :body",
+      ExpressionAttributeNames: {
+        "#title": "title",
+        "#body": "body",
+      },
+      ExpressionAttributeValues: {
+        ":title": data.title,
+        ":body": data.body,
+      },
+      ConditionExpression: "attribute_exists(notesId)",
+    };
+    await ddbDocClient.send(new UpdateCommand(params));
+    return send(200, data);
+  } catch (err) {
+    return send(500, err.message);
+  }
+};
 
 module.exports.deleteNote = async (event, context, cb) => {
   let notesId = event.pathParameters.id;
-  return {
-    statusCode: 201,
-    body: JSON.stringify(`delete new notes${notesId} successfully`),
-  };
+  try {
+    const params = {
+      TableName: NOTES_TABLE_NAME,
+      Key: { notesId },
+      ConditionExpression: "attribute_exists(notesId)",
+    };
+    await ddbDocClient.send(new DeleteCommand(params));
+    return send(200, notesId);
+  } catch (err) {
+    return send(500, err.message);
+  }
 };
 
 module.exports.getAllNotes = async (event, context, cb) => {
-  return {
-    statusCode: 201,
-    body: JSON.stringify(`get all notes`),
-  };
+  console.log(JSON.stringify(event));
+  try {
+    const params = {
+      TableName: NOTES_TABLE_NAME,
+    };
+    await ddbDocClient.send(new ScanCommand(params));
+    return send(200, notes);
+  } catch (err) {
+    return send(500, err.message);
+  }
 };
